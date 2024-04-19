003c242f4e92f7f154ab33fc6284d30be1469712
Evgeny Akulenko <AtlantisAJ@gmail.com>
2024-04-19 14:40:56 +0300

    finish project food

--- udemy-Ivan/preview/script.js
+++ udemy-Ivan/preview/script.js
@@ -1,31 +1,14 @@
-const btns = document.querySelectorAll('button');
-const wrapper = document.querySelector('#first');
+'use strict'
 
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(1));
-// console.log(btns[3].classList.add('red'))
-
-// btns[2].addEventListener('click', () => {
-//     // if (!btns[2].classList.contains('red')) {
-//     //     btns[2].classList.add('red')
-//     // } else {
-//     //     btns[2].classList.remove('red')
-//     // }
-//     btns[2].classList.toggle('red')
-// })
-
-btns.forEach((btn) => {
-    btn.addEventListener('', () => {
-        btn.classList.toggle('red')
-    })
-})
-
-wrapper.addEventListener('click', () => {
-    if (event.target && event.target.matches('button.red')) {
-        console.log('You press btn')
+const persone = {
+    name: 'Evgen',
+    tel: '3333333333',
+    parents: {
+        mom: 'Sveta',
+        dad: 'Mike'
     }
-})
+};
 
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
+console.log(JSON.stringify(persone));
+
+console.log(JSON.parse(JSON.stringify(persone)))
