a2fab4ecf3b6d2c6ccb3f1932f6818e4ffb4aa39
Evgeny Akulenko <AtlantisAJ@gmail.com>
2024-04-26 15:32:53 +0300

    remove food

--- udemy-Ivan/preview/script.js
+++ udemy-Ivan/preview/script.js
@@ -1,14 +1,33 @@
-'use strict'
+'use strict';
 
-const persone = {
-    name: 'Evgen',
-    tel: '3333333333',
-    parents: {
-        mom: 'Sveta',
-        dad: 'Mike'
+const data = [
+    {
+        id: 'box',
+        tag: 'div'
+    },
+    {
+        id: '',
+        tag: 'nav'
+    },
+    {
+        id: 'circle',
+        tag: ''
     }
-};
+];
 
-console.log(JSON.stringify(persone));
-
-console.log(JSON.parse(JSON.stringify(persone)))
+try {
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+    
+        if (!blockObj.id) {
+            throw new SyntaxError(`В данных под номером ${i + 1} нету уникального индентификатора`)
+        }
+    
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+    });
+} catch(e) {
+    if (e.name === 'SyntaxError') {
+        console.log(e.message);
+    } else throw e;
+}
